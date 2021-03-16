9f849c510d541f5789341846874aa87997ae617b
youwatanabeoku@yahoo.co.jp <ec2-user@ip-172-31-24-113.ec2.internal>
2021-03-16 14:56:41 +0000

    [Start]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '13c031637687d4a046933c704bbf05da334ac7c99b6159e6a33c85c90cd8bbcc5cc782de1588a3084e90db81e3d7a125109257423c0de8e06137a9030952e470'
